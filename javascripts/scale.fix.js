75cfdbef9d2cd7ca2a3612d5e1554c0d9b7b47d9
cliftonc <clifton.cunningham@gmail.com>
2012-05-26 14:56:18 +0100

    Added examples

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,31 @@ function gestureStart() {
     }
   }
 }
+
+// Global for cookies
+var cookieUtils = {
+    //From https://developer.mozilla.org/en/DOM/document.cookie
+    getItem: function (sKey) {
+      if (!sKey || !this.hasItem(sKey)) { return null; }
+      return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+    },         
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/.test(sKey)) { return; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (typeof vEnd) {
+          case "number": sExpires = "; max-age=" + vEnd; break;
+          case "string": sExpires = "; expires=" + vEnd; break;
+          case "object": if (vEnd.hasOwnProperty("toGMTString")) { sExpires = "; expires=" + vEnd.toGMTString(); } break;
+        }
+      }
+      document.cookie = escape(sKey) + "=" + escape(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    },
+    removeItem: function (sKey) {
+      if (!sKey || !this.hasItem(sKey)) { return; }
+      var oExpDate = new Date();
+      oExpDate.setDate(oExpDate.getDate() - 1);
+      document.cookie = escape(sKey) + "=; expires=" + oExpDate.toGMTString() + "; path=/";
+    },
+    hasItem: function (sKey) { return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie); }
+};
